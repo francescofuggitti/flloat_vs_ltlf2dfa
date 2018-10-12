3f06dd84e04673711f1f586a613bbe753c41e9f4
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2018-10-12 14:00:13 +0200

    fair time comparison

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ from timeit import Timer, repeat
 import gc
 import time
 
-#memory profiler
-from memory_profiler import memory_usage
-
 # set back-end for matplotlib on OSX
 import matplotlib as mpl
 mpl.use('TkAgg')
@@ -86,76 +83,19 @@ def plot_results_time(formulas, flloat_y_labels, ltlf2dfa_y_labels):
     ax.set_xticks(ind + width / 2)
     ax.set_xticklabels(formulas, rotation = 45, ha="right")
     ax.legend((rects1[0], rects2[0]), ('FLLOAT', 'LTLf2DFA'))
-
-    plt.show()
-
-def plot_results_memory(flloat_y_memories, ltlf2dfa_y_memories):
-
-    # x = np.arange(65)
-    x = np.arange(0., 15., 0.232)
-    plt.plot(x, flloat_y_memories, color='r')
-    plt.plot(x, ltlf2dfa_y_memories, color='b')
-    plt.xlabel('Time (s)')
-    plt.ylabel('Memory (MB)')
-    plt.title('Plot of memory')
-
-
-
-    # y = np.row_stack((flloat_y_memories, ltlf2dfa_y_memories))
-    #
-    # x = np.arange(65)
-    # y_stack = np.cumsum(y, axis=0)
-    #
-    # fig = plt.figure()
-    # ax1 = fig.add_subplot(111)
-    #
-    # ax1.plot(x, y_stack[0, :], label=1)
-    # ax1.plot(x, y_stack[1, :], label=2)
-    # ax1.legend(loc=2)
-    #
-    # colormap = plt.cm.gist_ncar
-    # colors = [colormap(i) for i in np.linspace(0, 1, len(ax1.lines))]
-    # for i, j in enumerate(ax1.lines):
-    #     j.set_color(colors[i])
-
     plt.show()
 
 if __name__ == '__main__':
     flloat_times = []
-    # flloat_memories = []
     ltlf2dfa_times = []
-    # ltlf2dfa_memories = []
-    # t0 = time.time()
     for formula in flloat_formulas:
         stmt = '{}(formula)'.format(functions[0])
         setp = 'from __main__ import formula, {}'.format(functions[0])
-        # flloat_times.append(timeit.timeit(stmt, setp, number=1000))
         flloat_times.append(min(repeat(stmt, setp, repeat=3, number=100)))
-        # flloat_memories += memory_usage(transform_to_dot_flloat(formula), interval=.2, timeout=1)
-    # t1 = time.time()
     gc.collect()
-    # t0_1 = time.time()
     for formula in ltlf2dfa_formulas:
         stmt = '{}(formula)'.format(functions[1])
         setp = 'from __main__ import formula, {}'.format(functions[1])
-        # ltlf2dfa_times.append(timeit.timeit(stmt, setp, number=1000))
-        # ltlf2dfa_times.append(timeit.Timer(stmt, setp).timeit(number=100) )
         ltlf2dfa_times.append(min(repeat(stmt, setp, repeat=3, number=100)))
-        # ltlf2dfa_memories += memory_usage(transform_to_dot_ltlf2dfa(formula), interval=.2, timeout=1)
-    # t1_1 = time.time()
-    #
-    # time = t1 - t0
-    # time_1 = t1_1 - t0_1
-    #
-    # print('time flloat: '+str(time))
-    # print('time ltlf2dfa: ' + str(time_1))
+
     plot_results_time(ltlf2dfa_formulas, flloat_times, ltlf2dfa_times)
-    #plot_results_memory(flloat_memories, ltlf2dfa_memories)
-    #print(len(ltlf2dfa_memories))
-    # t = np.arange(0., 15., 0.232)
-    # print(len(t))
-    # print(len(t**2))
-
-
-
-
